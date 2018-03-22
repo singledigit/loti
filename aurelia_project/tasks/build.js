7f8734283845cdb9d184c6edeec1d6fa12d5b498
Eric Johnson <ericj@singledigit.net>
2018-03-21 22:36:30 -0500

    Basic layout for framework

--- aurelia_project/tasks/build.js
+++ aurelia_project/tasks/build.js
@@ -44,8 +44,7 @@ function clearDist() {
 const build = gulp.series(
   clearDist,
   configureEnvironment,
-  buildWebpack,
-  prepareFontAwesome
+  buildWebpack
 );
 
 export {
--- src/app.js
+++ src/app.js
@@ -1,5 +1,15 @@
+import { PLATFORM } from 'aurelia-framework';
+
 export class App {
-  constructor() {
-    this.message = 'Hello World!';
+  configureRouter(config, router) {
+    this.router = router;
+    config.title = 'Life on the Inside';
+    config.map([
+      { route: ['', 'home'], name: 'home', moduleId: PLATFORM.moduleName('home/index'), nav: true, title: 'Home' },
+      { route: 'doula-services', name: 'doula', moduleId: PLATFORM.moduleName('doula/index'), nav: true, title: 'Doula Services' }
+      // { route: 'users', name: 'users', moduleId: 'users/index', nav: true, title: 'Users' },
+      // { route: 'users/:id/detail', name: 'userDetail', moduleId: 'users/detail' },
+      // { route: 'files/*path', name: 'files', moduleId: 'files/index', nav: 0, title: 'Files', href: '#files' }
+    ]);
   }
 }
